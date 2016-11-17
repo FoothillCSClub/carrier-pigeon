c33818ed4d23113190ea76b35c5bbd5a3767abb8
Grond <grond66@riseup.net>
2016-11-16 16:01:51 -0800

    Implement testing options in the client

--- client/carrier-pigeon-client.py
+++ client/carrier-pigeon-client.py
@@ -12,6 +12,7 @@ import readline
 from socket import *
 from selectors import *
 from carrier_pigeon_classes.message import *
+import argparse
 
 def fatal(msg):
     print(msg, file=sys.stderr)
@@ -21,18 +22,37 @@ def print_prompt():
     print("carrier-pigeon> ", end='', file=sys.stdout)
     sys.stdout.flush()
 
-connect_host = "foothillstemclubs.org"
-connect_port = 4545
+parser = argparse.ArgumentParser(description="Simple chat client")
+parser.add_argument("--port", type=int, default=4545, help="use a specific port [default 4545]")
+parser.add_argument("--host", type=str, default="foothillstemclubs.org", help="connect to a given host [default foothillstemclubs.org]")
+parser.add_argument("--server-test-mode", action="store_true", default=False, help="Run the client as a server instead (for testing purposes)")
+options = parser.parse_args()
 
 sock = socket()
+if options.server_test_mode:
+    try:
+        # allow for quick reuse of the address
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind(("localhost", options.port))
+        sock.listen(1)
+        print("listening on port " + str(options.port) + "...")
+        (sock, peeraddr) = sock.accept()
+        print("client connected from " + peeraddr[0] + ":" + str(peeraddr[1]))
+    except BaseException as e:
+        exit("Error waiting for connection on port " + str(options.port) + ": " + str(e))
+else:
+    try:
+        # set network timeout to something sane
+        sock.connect((options.host, options.port))
+    except BaseException as e:
+        fatal("Error connecting to " + options.host + ":" + str(options.port) + ": " + str(e))
+
 try:
-    # set network timeout to something sane
     sock.settimeout(5)
-    sock.connect((connect_host, connect_port))
     network_send = sock.makefile(mode="w", encoding="utf-8", buffering=4096)
     network_recv = sock.makefile(mode="r", encoding="utf-8", buffering=4096)
 except BaseException as e:
-    fatal("Error connecting to " + connect_host + ":" + str(connect_port) + ": " + str(e))
+    exit("Error setting socket options or creating network buffers: " + str(e))
 
 io_selector = DefaultSelector()
 io_selector.register(sys.stdin, EVENT_READ)
